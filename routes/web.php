a85067cb41177f4131ce71c6e5fee8d372811eee
Hasnain- Ullah <hasnainkhanxd21@gmail.com>
2025-09-21 21:43:27 -0700

    Route related

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Home Page Route
+Route::get('/', function () {  // Default route
+    return view('welcome');    // Route is an inbuilt class and get is a static method to use
+});                            // get we use scope resolution operator (::)
+
+Route::get('/about' , function(){  // we can also return html file
+    return "<h1> About Page </h1>";
+});
+
+Route::view('/hello','post');  // we give two two parameters first is route_name and 2nd is view_name
+
+Route::get('/post/{id?}/{commentID?}', function(string $id = null , string $commentId = null){
+    if($id){
+        return "<h2>Post ID : ".$id."& Comment ID : ".$commentId."</h2>";
+    }else{
+        return "<h2>No Post ID Found</h2>";
+    }
 });
